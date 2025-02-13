3c7628b6362833809aee5764c75f8ce73d7a91a8
Shaun Stummer <shaun.stummer1@tafensw.edu.au>
2025-02-13 13:16:00 +1100

    Updated with week 1 case study review

--- OtherEx/Program.cs
+++ OtherEx/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+
+var totalOutputString = new StringBuilder();
+totalOutputString.AppendLine("Hello World!");
+totalOutputString.Append("See what happens###");
+totalOutputString.AppendLine(" when we appendline###");
+totalOutputString.Append(" Something cool8787234736");
+
+totalOutputString.Insert(0, "This is being added to the first line8787234736");
+totalOutputString.Remove(10, 23);
+totalOutputString.Replace("8787234736", "***********");
+totalOutputString.Replace("###", "?");
+
+Console.WriteLine(totalOutputString.ToString());
+
+Console.ReadKey();
---
+++ Week1CaseStudyReview/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Bob's program to manage information about cars
+        string carBrand = "Toyota"; // Added missing semicolon
+
+        Console.WriteLine("Enter the model of your car:");
+        string carModel = Console.ReadLine();
+
+        Console.WriteLine("Enter the year of manufacture of your car:");
+
+        // Ensuring valid integer input
+        int carYear;
+        while (!int.TryParse(Console.ReadLine(), out carYear) || carYear > DateTime.Now.Year || carYear < 1886)
+        {
+            Console.WriteLine("Invalid input. Please enter a valid year:"); // Prevents unrealistic years
+        }
+
+        int carAge = DateTime.Now.Year - carYear; // Corrected calculation, ensuring valid subtraction
+
+        // Displaying car information
+        Console.WriteLine("\nCar Information:");
+        Console.WriteLine("Car Brand: " + carBrand);
+        Console.WriteLine("Car Model: " + carModel);
+        Console.WriteLine("Car Age: " + carAge + " years old");
+
+        // Example method to get a list of cars
+        GetListOfCars();
+    }
+
+    static void GetListOfCars()
+    {
+        Console.WriteLine("\nList of Example Cars:");
+        Console.WriteLine("1. Toyota Camry");
+        Console.WriteLine("2. Honda Accord");
+        Console.WriteLine("3. Ford Mustang");
+    }
+}
